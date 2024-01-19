46d4a908abf4a7443df593687cb7f9ad09ccb7f2
Zoe-Soen <125407606+Zoe-Soen@users.noreply.github.com>
2024-01-20 07:09:31 +0900

    Update config.py
    Moved sensitive info to .env

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # encoding: utf-8
 from datetime import datetime as dt, timedelta
 
+
 # ---------------- Flight Seartch API access Info ----------------
 KIWI_ENDPOINT = "https://tequila-api.kiwi.com"
-API_KEY = "" # Register your own API KEY and replace
 
 
 # -------------- City-From-To / Currency / Time-Frame --------------
@@ -16,24 +16,15 @@ SEARCH_TIMEFRAME = dt.now() + timedelta(days=(6 * 30))
 
 
 # ------------------- Mail Notification settings -------------------
-# export MAIL_SENDER=xxxxxxx@xxxx.com
-# export MAIL_SENDER_PW=xxxxxxxxxxxxxxxxxxxxxx
-SEND_TO = "" # To multi persons: ["email_1", "email_2",...,"email_n"]
 MAIL_FOLDER = "./assets/sent_mail_records"
 MAIL_RECODE_FILE = f"{MAIL_FOLDER}/{dt.now().strftime('%Y%m%d')}.txt"
 
-# ---------------- Mobile SMS Notification settings ----------------
-# export TWILIO_ACCOUNT_SID=xxxxxxxxxxxxxxxxxxxxxx
-# export TWILIO_AUTH_TOKEN=xxxxxxxxxxxxxxxxxxxxxx
-# export TWILIO_VIRTUAL_NUMBER=+0000000000
-# export TWILIO_VERIFIED_NUMBER=+810000000000
-
 
 # -------------------- Google Sheets API Info --------------------
-GOOGLE_SHEEET_URL = "https://docs.google.com/spreadsheets/d/1Ua7jD6dcvPUoj5l_VbjmOIV7tl2Adm6OrSVQhywBzJE/edit#gid=1484169761"
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
 SERVICE_ACCOUNT = "./assets/wish-list-tracer-79fea93c45dd.json"  # Replace your own Google Spreadsheet Service Account Key
-SHEET_ID = "1Ua7jD6dcvPUoj5l_VbjmOIV7tl2Adm6OrSVQhywBzJE"   # Replace sheet id
+SHEET_ID = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"   # Replace sheet id
+GOOGLE_SHEEET_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}"
 WORKSHEET1 = "list" #Sheet name for the Target Cities and Maximum Allowable Prices list
 WORKSHEET2 = "price_records"  # Sheet name for all search records
 WORKSHEET3 = "sin-bkk" # Sheet name for search record of particular city
